3e5718a6a6ff593e7db48e8f1ebda96852501ae7
RodionMalkov <rodionmalkov@gmail.com>
2024-02-10 17:28:02 +0500

    start section team

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ import 'swiper/css/pagination';
 
 import '../stylesheets/index.scss';
 
-var swiper = new Swiper(".office", {
+let officeСarousel = new Swiper(".office", {
   slidesPerView: 1,
   pagination: {
     el: ".swiper-pagination",
@@ -28,6 +28,28 @@ var swiper = new Swiper(".office", {
     },
     1700: {
       slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
+
+let teamСarousel = new Swiper(".team", {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: "auto",
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1700: {
+      slidesPerView: 4,
       spaceBetween: 40,
     },
   },
