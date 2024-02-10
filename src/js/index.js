b147b822599b625ec435520d617588e7db7f27a6
RodionMalkov <rodionmalkov@gmail.com>
2024-02-10 13:41:08 +0500

    start section office

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 import '../index.html';
 import '../stylesheets/index.scss';
+
+import Swiper from 'swiper';
+import 'swiper/css';
+
+var swiper = new Swiper(".office", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
